b4f5bd6aef7c6c8564cfa214ba7f666cd5c4fd22
canlas1 <dc@uscr.com>
2017-03-16 00:44:56 -0400

    push5

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
     console.log("ready!");
 
-    var userScore = 0;
 
 
-    var body = document.getElementsByTagName("body");
-    console.log(body);
+    // var body = document.getElementsByTagName("body");
+    // console.log(body);
     // user presses start button 
     // screen switched to a trivia game and the user has a set amount of time
     // trivia game starts
@@ -26,50 +25,38 @@ $(document).ready(function() {
 
     $("#start").click(function() { // when you click start do this code this is annonymus function
         $("#container").show(); // show the questions
-
-
         quizTime.start();
-
-
     });
 
     var quizTime = {
-
         time: 60,
 
         reset: function() {
-
             quizTime.time = 60;
-
-
             // DONE: Change the "display" div to "00:00."
             $("#display").html("01:00");
-
         },
+
         start: function() {
             console.log("starting timer");
-
             // DONE: Use setInterval to start the count here.
             intervalId = setInterval(quizTime.decrement, 1000);
         },
-        stop: function() {
 
+        stop: function() {
             // DONE: Use clearInterval to stop the count here.
             clearInterval(intervalId);
         },
 
         decrement: function() {
             console.log(quizTime.time);
-
             // DONE: increment time by 1, remember we cant use "this" here.
             quizTime.time--;
             if (quizTime.time === 0) {
                 quizTime.stop();
-
             }
 
-
-            // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
+            // DONE: Get the current time, pass that into the quizTime.timeConverter function,
             //       and save the result in a variable.
             var converted = quizTime.timeConverter(quizTime.time); //taking the time in seconds and calls timeConverter function which is a method inside of quizTIme
             console.log(converted);
@@ -77,74 +64,136 @@ $(document).ready(function() {
             // DONE: Use the variable we just created to show the converted time in the "display" div.
             $("#display").html(converted);
         },
-        timeConverter: function(t) {
 
+        timeConverter: function(t) {
             var minutes = Math.floor(t / 60);
             var seconds = t - (minutes * 60);
-
             if (seconds < 10) {
                 seconds = "0" + seconds;
             }
-
             if (minutes === 0) {
                 minutes = "00";
             } else if (minutes < 10) {
                 minutes = "0" + minutes;
             }
-
             return minutes + ":" + seconds;
         }
-    };
-
+    }; //closes quizTime
     console.log(quizTime);
+
+
     console.log("does this work")
 
-    var myButton = document.getElementById("myButton");
-    console.log(myButton);
-
-    $("#myButton").on("click", function() {
-        parsChildren()
-        console.log(this)
-        console.log(this.type)
-        console.log(this.id)
-        console.log(correctAns.value);
-        
 
 
 
-    });
 
     
+    // console.log(this.type)
+    // console.log(this.id)
 
-    var question1 = document.getElementById("question1");
-    console.log(question1);
-    console.log(question1.children[1].children);
+    // function onSubmit(){ //when user hits submit once quiz is complete
+    //     var userScore = 0;
+    //     var correctAns = ["1"]
 
-    if (correctAns.checked === question1.children[1].children) {
-            userScore++;
-        }
+    // var q1 = document.forms["quiz"]["q1"].value;
+    // var q2 = document.forms["quiz"]["q2"].value;
+    // var q3 = document.forms["quiz"]["q3"].value;
+    // var q4 = document.forms["quiz"]["q4"].value;
+    // var q5 = document.forms["quiz"]["q5"].value;
+    // var q6 = document.forms["quiz"]["q6"].value;
+    // var q7 = document.forms["quiz"]["q7"].value;
 
+    // console.log (q1);
+    // console.log (q2);
+    // console.log (q3);
+    // console.log (q4);
+    // console.log (q5);
+
+    // $("#score").html 
+    // if (correctAns.checked) === q1.answersArr[2]{
+    //     userScore++;
+    // }
+    // }
+// var answerArr = [1]    
+// var correctAns = ["1"];
     
-    var question2Arr = question2.children[1].children;
-    var question3Arr = question3.children[1].children;
-    var question4Arr = question4.children[1].children;
-    var question5Arr = question5.children[1].children;
-    var question6Arr = question6.children[1].children;
-    var question6Arr = question7.children[1].children;
-    
+ 
+    function onSubmit() {
 
-    var parsChildren = function() {
-        for (var i = 0; i < question1Arr.length; i++) {
-            console.log(question1Arr[i]);
-            if (question1Arr[i].checked) {
-                console.log("i am checked");
-                console.log(question1Arr[i]);
+        var correctAns = 0;
+        var wrongAnswers = 0;
+        $(".correct").html("Correct Answers: " + correctAns);
+        $(".wrong").html("Wrong Answers: " + wrongAnswers);
+        for (var i = 0; i < correctAns.length; i++) {
+
+            var radios = $(".name")
+            if (radio.value === "[1]" && radio.checked) {
+                correctAns++
+            } else if (radio.value === "wrong" && radio.checked) {
+                wrongAnswers++
+
 
             }
+
+
+     $("#myButton").on("click") // my button on click jQuery
+    console.log("this is working BUTTON");
+    //     parsChildren()
+    console.log(this)
+
         }
     }
 
-    parsChildren()
+        
+
+       //   $(".answer").click(function){ //from the radio input
+       // answer += parseInt($(this).attr("value"));}
+       
+
+
+        
+
+        
+
+
+    // }
+    //     console.log(onSubmit());
+
+
+    // });
+
+
+
+    // var question1 = document.getElementById("question1");
+    // console.log(question1);
+    // console.log(question1.children[1].children);
+
+    // if (correctAns.checked === question1.children[1].children) {
+    //         userScore++;
+    //     }
+
+
+    // var question2Arr = question2.children[1].children;
+    // var question3Arr = question3.children[1].children;
+    // var question4Arr = question4.children[1].children;
+    // var question5Arr = question5.children[1].children;
+    // var question6Arr = question6.children[1].children;
+    // var question6Arr = question7.children[1].children;
+
+
+    // var parsChildren = function() {
+    //  for (var i = 0; i < question1Arr.length; i++) {
+    //         console.log(question1Arr[i]);
+    //         if (question1Arr[i].checked) {
+    //             console.log("i am checked");
+    //             console.log(question1Arr[i]);
+
+    //         }
+    //     }
+    // }
+
+    // parsChildren()
 
 
 
